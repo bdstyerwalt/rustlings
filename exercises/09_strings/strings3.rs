2e2196c2a267ab0009991828d735adb09328d967
bdstyerwalt <150213199+bdstyerwalt@users.noreply.github.com>
2023-11-26 06:23:42 +0000

    Caught up and finished through modules

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,21 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let s = &String::from(input);
+    return s.trim().to_string();
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    return String::from(input) + " world!";
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let s = &String::from(input);
+    return s.replace("cars", "balloons")
 }
 
 #[cfg(test)]
